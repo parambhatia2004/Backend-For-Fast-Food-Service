6566a7cf25707636003eb884acb780101d303bc6
parambhatia2004 <98880078+parambhatia2004@users.noreply.github.com>
2022-10-03 16:46:56 +0100

    Add files via upload

---
+++ Pre-Existing Code/ICustomer.java
@@ -0,0 +1,22 @@
+package uk.ac.warwick.cs126.interfaces;
+
+import java.util.Date;
+
+public interface ICustomer {
+
+    Long getID();
+    String getStringID();
+    String getFirstName();
+    String getLastName();
+    Date getDateJoined();
+    float getLatitude();
+    float getLongitude();
+
+    void setID(Long id);
+    void setFirstName(String firstName);
+    void setLastName(String lastName);
+    void setDateJoined(Date dateJoined);
+    void setLatitude(float latitude);
+    void setLongitude(float longitude);
+
+}
---
+++ Pre-Existing Code/IFavourite.java
@@ -0,0 +1,18 @@
+package uk.ac.warwick.cs126.interfaces;
+
+import java.util.Date;
+
+public interface IFavourite {
+
+    Long getID();
+    String getStringID();
+    Long getCustomerID();
+    Long getRestaurantID();
+    Date getDateFavourited();
+
+    void setID(Long id);
+    void setCustomerID(Long customerID);
+    void setRestaurantID(Long restaurantID);
+    void setDateFavourited(Date dateFavourited);
+
+}
